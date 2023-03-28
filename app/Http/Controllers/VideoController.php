e19358a75902adaa448db70582e6f852917ed133
Mukhammadali <ali_6017@mail.ru>
2023-03-28 23:22:53 +0500

    VideoController method code refactor

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -3,64 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Video;
-use Illuminate\Http\Request;
 
 class VideoController extends Controller
 {
     public function index()
     {
-        switch (request('period')) {
-            case 'year':
-                $date = now()->startOfYear();
-                break;
-            case 'month':
-                $date = now()->startOfMonth();
-                break;
-            case 'week':
-                $date = now()->startOfWeek();
-                break;
-            case  'day':
-                $date = now()->startOfDay();
-                break;
-            case 'hour':
-                $date = now()->startOfHour();
-                break;
-            default:
-                $date = null;
-        }
-
-//        if ($date) {
-//            return Video::where('created_at', '>', $date)->count();
-//        }
-//        return Video::count();
+        $date = match ($request('period')) {
+            'year' => now()->startOfYear(),
+            'month' => now()->startOfMonth(),
+            'week' => now()->startOfWeek(),
+            'day' => now()->startOfDay(),
+            'hour' => now()->startOfHour(),
+            default => null
+        };
 
         return $date
             ? Video::where('created_at', '>', $date)->get()
             : Video::get();
-
-//-------------------------------------------------------------------------------------
-//        return Video::with('channel', 'categories')->get();
-//        return Video::where('created_at', '>', now()->startOfYear())->get();
-
-//--------------------------------------------------------------------------------------
-//_______________________________________________________________________________________
-//        return [
-//           'year' => Video::where('created_at', '>', now()->startOfYear())->count(),
-//           'month' => Video::where('created_at', '>', now()->startOfMonth())->count(),
-//           'week' => Video::where('created_at', '>', now()->startOfWeek())->count(),
-//           'day' => Video::where('created_at', '>', now()->startOfDay())->count(),
-//           'hour' => Video::where('created_at', '>', now()->startOfHour())->count(),
-//        ];
-
-//______________________________________________________________________________________
-//        return [
-//            'year' => Video::where('created_at', '>', now()->startOfYear())->get(),
-//            'month' => Video::where('created_at', '>', now()->startOfMonth())->get(),
-//            'week' => Video::where('created_at', '>', now()->startOfWeek())->get(),
-//            'day' => Video::where('created_at', '>', now()->startOfDay())->get(),
-//            'hour' => Video::where('created_at', '>', now()->startOfHour())->get(),
-//        ];
     }
+
 
     public function show(Video $video)
     {
